9fb86a2d01abac1cbca9ecfd9719b88e1d8f0497
John Doe <sylvieberger@web.de>
2019-07-22 18:37:53 +0200

    images added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
     componentDidMount() {
         axios
-            .get('https://jsonplaceholder.typicode.com/todos?_limit=4')
+            .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
             .then(res => this.setState({ todos: res.data }));
     }
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ function About() {
         <React.Fragment>
             <div className="about-txt">
                 <h3>About</h3>
-                <p>This is a To Do List made with React.</p>
+                <p>This is a To Do List made with React 16.8.6</p>
             </div>
         </React.Fragment>
     );
